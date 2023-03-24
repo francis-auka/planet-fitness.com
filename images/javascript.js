1c8f2a86ffb7b5c4c8485ce30ae1b553d9e6afa5
francis-auka <127690842+francis-auka@users.noreply.github.com>
2023-03-24 15:00:51 +0300

    Add files via upload

---
+++ images/javascript.js
@@ -0,0 +1,11 @@
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".popup").style.display="flex"})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";})
+
+document.getElementById("BUTN").addEventListener("click", function(){
+    document.querySelector(".pop-up").style.display="flex"})
+
+document.querySelector(".shutdown").addEventListener("click", function(){
+    document.querySelector(".pop-up").style.display = "none";})
